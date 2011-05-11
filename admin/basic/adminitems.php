f282981ac199f9153e939ff3697976a82d15d7d6
RPG84 <RPG84@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-05-11 10:34:21 +0000

    git-svn-id: https://plugins.svn.wordpress.org/tradetracker-store/tags/2.1@383553 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ admin/basic/adminitems.php
@@ -0,0 +1,235 @@
+<?php
+function adminstore_items()
+{
+	global $wpdb;
+	if( get_option( Tradetracker_update ) == "" ){
+		$update= "24";
+	} else {
+		$update= get_option( Tradetracker_update );
+	}
+	$Tradetracker_xml = get_option( Tradetracker_xml );
+	if ($Tradetracker_xml == null) 
+	{
+		echo "No XML filled in yet please change the settings first.";
+	} else {
+	$context = stream_context_create(array(
+    'http' => array(
+        'timeout' => 3      // Timeout in seconds
+    )
+	));
+		$cache_time = 3600*$update; // 24 hours
+		$cache_file = WP_PLUGIN_DIR . '/tradetracker-store/cache.xml';
+		$timedif = @(time() - filemtime($cache_file));
+		if (file_exists($cache_file) && $timedif < $cache_time) 
+		{
+			if ('' == file_get_contents($cache_file))
+				{
+		     			$string = file_get_contents(''.$Tradetracker_xml.'', 0, $context);
+		    			if ($f = @fopen($cache_file, 'w')) {
+        					fwrite ($f, $string, strlen($string));
+        					fclose($f);
+    					}
+					fill_database();
+				}  
+
+		} else {
+    			$string = file_get_contents(''.$Tradetracker_xml.'', 0, $context);
+    			if ($f = @fopen($cache_file, 'w')) {
+        			fwrite ($f, $string, strlen($string));
+        			fclose($f);
+    			}
+			fill_database();
+		}
+	return adminshow_items();
+	}
+}
+
+function adminshow_items()
+{
+	if (!current_user_can('manage_options'))
+	{
+		wp_die( __('You do not have sufficient permissions to access this page.') );
+	}
+    	if( isset($_POST['posted']) && $_POST['posted'] == 'Y' ) 
+	{
+                $Tradetracker_items = $_POST['item'];
+		$Tradetracker_items = implode(",", $Tradetracker_items);
+		if($_POST['itemsother']!="")
+		{
+			$Tradetracker_items = $Tradetracker_items.",".$_POST['itemsother'];
+		}
+		if ( get_option(Tradetracker_productid)  != $Tradetracker_items) 
+		{
+			update_option(Tradetracker_productid, $Tradetracker_items );
+  		}
+		echo "<div class=\"updated\"><p><strong>Settings Saved</strong></p></div>";	
+	}
+	if($_GET['order']==null)
+	{
+		$order = "name";
+	} else {
+		$order = $_GET['order'];
+	}
+	global $wpdb; 
+	$limit = $_GET['limit'];
+	$currentpage = $_GET['currentpage'];
+	if (!($limit))
+	{
+		$limit = 100;
+	}
+	if (!($currentpage)){
+		$currentpage = 0;
+	}
+	$table = PRO_TABLE_PREFIX."store";
+	$countquery=$wpdb->get_results("SELECT * FROM ".$table."");
+	$numrows = $wpdb->num_rows;
+	$pages = intval($numrows/$limit); // Number of results pages.
+	if ($numrows%$limit) 
+	{
+		$pages++;
+	} 
+	$current = ($currentpage/$limit) + 1;
+	if (($pages < 1) || ($pages == 0)) 
+	{
+		$total = 1;
+	} else {
+		$total = $pages;
+	} 
+	$first = $currentpage + 1;
+	if (!((($currentpage + $limit) / $limit) >= $pages) && $pages != 1) 
+	{
+		$last = $currentpage + $limit;
+	} else {
+		$last = $numrows;
+	}
+?>
+<style type="text/css" media="screen">
+#screenshot{
+	position:absolute;
+	border:1px solid #ccc;
+	background:#333;
+	padding:5px;
+	display:none;
+	color:#fff;
+	max-width:400px;
+	max-height:400px;
+	}
+
+/*  */
+</style>
+<?php
+	echo '<div class="wrap">';
+	echo "<h2>" . __( 'Tradetracker Store Setup', 'menu-test' ) . "</h2>";
+	$file = WP_PLUGIN_DIR . '/tradetracker-store/store.css';
+	$file_directory = dirname($file);
+	if(is_writable($file_directory)){
+	} else {
+		echo "<div class=\"updated\"><p><strong>Please make sure the directory ".$file_directory."/ is writable.</strong></p></div>";
+	}
+
+?>
+<ul class="tabset_tabs">
+   <li><a href="admin.php?page=tradetracker-shop#tab1">Setup</a></li>
+   <li><a href="admin.php?page=tradetracker-shop-settings#tab2">Settings</a></li>
+   <li><a href="admin.php?page=tradetracker-shop-items#tab3" class="active">Items</a></li>
+   <li><a href="admin.php?page=tradetracker-shop-overview#tab4">Overview</a></li>
+   <li><a href="admin.php?page=tradetracker-shop-feedback#tab5">Feedback</a></li>
+   <li><a href="admin.php?page=tradetracker-shop-help#tab6" class="redhelp">Help</a></li>
+</ul>
+	<div id="sideblock" style="float:right;width:200px;margin-left:10px;border:1px;position:relative;border-color:#000000;border-style:solid;"> 
+		<iframe width=200 height=800 frameborder="0" src="http://debestekleurplaten.nl/tradetracker-store/news.php"></iframe>
+ 	</div>
+<div id="tab3" class="tabset_content">
+   <h2 class="tabset_label">Items</h2>
+<table width="700" border="0">
+	<tr>
+		<td width="50%" align="left">
+			Showing products <b><? echo $first; ?></b> - <b><?php echo $last; ?></b> of <b><?php echo $numrows; ?></b>
+  		</td>
+  		<td width="50%" align="right">
+			Page <b><?php echo $current; ?></b> of <b><?php echo $total; ?></b>
+  		</td>
+ 	</tr>
+ 	<tr>
+  		<td colspan="2" align="right">
+			Results per-page: <a href="admin.php?page=tradetracker-shop-items&order=<?php echo $order; ?>&currentpage=<?php echo $currentpage; ?>&limit=100">100</a> | <a href="admin.php?page=tradetracker-shop-items&order=<?php echo $order; ?>&currentpage=<?php echo $currentpage; ?>&limit=200">200</a> | <a href="admin.php?page=tradetracker-shop-items&order=<?php echo $order; ?>&currentpage=<?php echo $currentpage; ?>&limit=500">500</a> | <a href="admin.php?page=tradetracker-shop-items&order=<?php echo $order; ?>&currentpage=<?php echo $currentpage; ?>&limit=1000">1000</a>
+  		</td>
+ 	</tr>
+</table>
+<?php
+$visits=$wpdb->get_results("SELECT * FROM ".$table." ORDER BY ".$order." ASC LIMIT ".$currentpage.", ".$limit."");
+	echo "<table width=\"700\">";
+		echo "<tr><td width=\"100\">";
+			echo "<b><a href=\"admin.php?page=tradetracker-shop-items&order=productID\">ProductID</a></b>";
+		echo "</td><td width=\"480\">";
+			echo "<b><a href=\"admin.php?page=tradetracker-shop-items&order=name\">Product name</a></b>";
+		echo "</td><td width=\"50\">";
+			echo "<b><a href=\"admin.php?page=tradetracker-shop-items&order=price\">Price</a></b>";
+		echo "</td><td width=\"70\">";
+			echo "<b>Currency</b>";
+		echo "</td></tr>";
+	echo "<form name=\"form2\" method=\"post\" action=\"\">";
+		echo "<input type=\"hidden\" name=\"posted\" value=\"Y\">";
+			$array2="";
+			foreach ($visits as $product){
+				$array2 .= ",".$product->productID."";
+				echo "<tr><td>";
+				$productID = get_option( Tradetracker_productid );
+				$productID = explode(",",$productID);
+				if(in_array($product->productID, $productID, true))
+				{
+					echo "<input type=\"checkbox\" checked=\"yes\" name=\"item[]\" value=".$product->productID." />";
+				} else {
+					echo "<input type=\"checkbox\" name=\"item[]\" value=".$product->productID." />";
+				}
+			echo $product->productID;
+		echo "</td><td><a href=\"#thumb\" class=\"screenshot\" rel=\"".$product->imageURL."\">";
+			echo $product->name;
+		echo "</a></td><td>";
+			echo $product->price;
+		echo "</td><td>";
+			echo $product->currency;
+		echo "</td></tr>";
+
+
+}
+		if(!empty($array2)){
+			$array1 = $productID;
+			$array2 = explode(",", $array2);
+			$result = array_diff($array1, $array2);
+			$result = implode(",", $result);
+		}
+			echo "<input type=\"hidden\" name=\"itemsother\" value=\"".$result."\" />";
+	echo "</table>";
+	echo "<p class=\"submit\">";
+	?>
+	<b>Always save changes before pressing next.</b><br>
+		<input type="submit" name="Submit" class="button-primary" value="<?php esc_attr_e('Save Changes') ?>" /> 	
+	<?php
+			if (get_option( Tradetracker_settings )==1){ ?> 
+				<INPUT type="button" name="Next" value="<?php esc_attr_e('Next') ?>" onclick="location.href='admin.php?page=tradetracker-shop-overview'"> 
+			<?php } ?>
+		<INPUT type="button" name="Help" value="<?php esc_attr_e('Help') ?>" onclick="location.href='admin.php?page=tradetracker-shop-help#help4'">
+<?php
+	echo "</p>";
+	echo "</form>";
+echo "<table width=\"700\"><tr><td>";
+	if ($currentpage != 0) { // Don't show back link if current page is first page.
+		$back_page = $currentpage - $limit;
+			echo("<a href=\"admin.php?page=tradetracker-shop-items&order=$order&currentpage=$back_page&limit=$limit\">back</a>    \n");}
+
+			for ($i=1; $i <= $pages; $i++) // loop through each page and give link to it.
+			{
+				$ppage = $limit*($i - 1);
+					if ($ppage == $currentpage){
+						echo("<b>$i</b> \n");} // If current page don't give link, just text.
+					else{
+						echo("<a href=\"admin.php?page=tradetracker-shop-items&order=$order&currentpage=$ppage&limit=$limit\">$i</a> \n");}
+					}
+					if (!((($currentpage+$limit) / $limit) >= $pages) && $pages != 1) { // If last page don't give next link.
+						$next_page = $currentpage + $limit;
+						echo("    <a href=\"admin.php?page=tradetracker-shop-items&order=$order&currentpage=$next_page&limit=$limit\">next</a>\n");}
+						echo "</td></tr></table>";
+	echo "</div></div>";
+}
+?>
---
+++ readme.txt
@@ -0,0 +1,87 @@
+=== Plugin Name ===
+Contributors: RPG84
+Donate link: none
+Tags: tradetracker, store, productfeed, affiliate, generate income, advertisements
+Requires at least: 2.0.1
+Tested up to: 3.1.2
+Stable tag: 2.1.0
+
+A plugin that lets you create a store from the Tradetracker productfeed.
+
+== Description ==
+
+This plugin gives you the abillity to add a store to your WordPress, based on a tradetracker productfeed. Tradetracker is an affiliate system that has
+the abillity to generate a product feed for you. So you can have a store that brings in money without the hassle of owning a complete webstore. All you need to do is choose a store connected on tradetracker and add it. Users of your blog will then see the products on your blog and when interested they will be sent to the store. When they buy an item you will get a percentage of that sale.
+
+Plugin also supports Lightbox. So if you don't have it yet i would advise to install http://wordpress.org/extend/plugins/wp-jquery-lightbox/
+
+== Installation ==
+
+1. Upload `plugin-name.php` to the `/wp-content/plugins/` directory.
+1. Activate the plugin through the 'Plugins' menu in WordPress.
+1. Adjust all settings in the TT Store on the left side of the admin menu
+
+== Frequently Asked Questions ==
+
+= Have you gotten any questions yet? =
+
+No, not yet.
+
+= Why can't I do certain things with the plugin? =
+
+I've created functions for this plugin of which I thought they'd be useful, but ofcourse I may have left things out which other people would like to see. If you have any ideas, do share them with me, as I can always try to implement it.
+
+== Screenshots ==
+
+1. This is how the items will be shown on your site
+2. This is the settings menu in the admin area
+3. Here you select which products you want to show on your site
+
+== Changelog ==
+= 2.1.0 =
+Mostly graphical updates for the admin menu. Also improved the help pages.
+
+= 2.0.5 =
+Small adjustment to accept more productfeeds from tradetracker
+
+= 2.0.4 =
+Some redundant error messages removed
+
+= 2.0.3 =
+Made the databasefield for the price a bit longer.
+
+= 2.0.2 =
+Some minor fixes for the basic store
+
+= 2.0.1 = 
+1. Completely redid the whole menu 
+1. Stats in dashboard
+1. Multistore (still with one XML) possibility
+1. Multiple layouts possible
+
+= 1.4.1 =
+Improved algoritm for importing XML
+
+= 1.4 =
+1. Added ability to manually choose the Font used in the layout settings
+1. Added a extra check for when cache.xml stayed empty
+1. Added an option in setting screen where you can decide how often an new xml file is downloaded
+
+= 1.3.6 = 
+Small bug solved in the item selection screen
+
+= 1.3.5 = 
+
+Made the use of Lightbox optional
+
+= 1.3.3 = 
+
+Somehow only the shortcode was working. Now it is also working in your template again.
+
+= 1.3.2 =
+
+Fixed minor issue when updating
+
+= 1.3 =
+
+In Tt Store Layout you will be able to adjust the layout. You can change the width and the colors
