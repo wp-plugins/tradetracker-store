5d6b298233193c32ef768aa1a6a0b281e1eccabc
RPG84 <RPG84@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-07-29 14:39:04 +0000

    git-svn-id: https://plugins.svn.wordpress.org/tradetracker-store/tags/2.1.9@416945 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ admin/basic/adminoverview.php
@@ -0,0 +1,112 @@
+<?php
+function format_bytes($size) {
+    $units = array(' B', ' KB', ' MB', ' GB', ' TB');
+    for ($i = 0; $size >= 1024 && $i < 4; $i++) $size /= 1024;
+    return round($size, 2).$units[$i];
+}
+
+function tradetracker_store_overview() {
+global $wpdb;
+$pro_table_prefix=$wpdb->prefix.'tradetracker_';
+$tablemulti = PRO_TABLE_PREFIX."multi";
+$tablelayout = PRO_TABLE_PREFIX."layout";
+define('PRO_TABLE_PREFIX', $pro_table_prefix);
+
+	echo '<div class="wrap">';
+	if (get_option(Tradetracker_settings)==1){
+			
+?>
+	<h2>Overview of selected options:</h2>
+<div class="plugindiv">
+<ul class="tabset_tabs">
+   <li><a href="admin.php?page=tradetracker-shop#tab1">Setup</a></li>
+   <li><a href="admin.php?page=tradetracker-shop-settings#tab2">Settings</a></li>
+   <li><a href="admin.php?page=tradetracker-shop-items#tab3">Items</a></li>
+   <li><a href="admin.php?page=tradetracker-shop-overview#tab4" class="active">Overview</a></li>
+   <li><a href="admin.php?page=tradetracker-shop-feedback#tab5">Feedback</a></li>
+   <li><a href="admin.php?page=tradetracker-shop-help#tab6" class="redhelp">Help</a></li>
+</ul>
+
+<div id="tab4" class="tabset_content">
+   <h2 class="tabset_label">Overview</h2>
+	<table width="700">
+		<tr>
+			<td width="150">
+				Selected XML File:
+			</td>
+			<td rowspan="2">
+				<a href="<?php echo get_option( Tradetracker_xml ); ?>">Click here to open XML</a>
+			</td>
+
+		</tr>
+		<tr>
+			<td>
+			</td>
+		</tr>
+		<tr>
+			<td width="150">
+				XML File size:
+			</td>
+			<td>
+				<?php $filename = WP_PLUGIN_DIR . '/tradetracker-store/cache.xml';	if (file_exists($filename)) {echo format_bytes(filesize($filename));} else { echo "Not calculated yet"; } ?>
+			</td>
+
+		</tr>
+		<tr>
+			<td width="150">
+				Monthly bandwidth:
+			</td>
+			<td>
+				<?php $filename = WP_PLUGIN_DIR . '/tradetracker-store/cache.xml';	if (file_exists($filename)) {echo format_bytes(30*filesize($filename));} else { echo "Not calculated yet"; } ?>
+			</td>
+
+		</tr>
+		<?php if (get_option( Tradetracker_productid )==""){ ?>
+		<tr>
+			<td width="150">
+				Amount of items:
+			</td>
+			<td>
+				<?php if (get_option( Tradetracker_amount )=="") { echo "12"; } else { echo get_option( Tradetracker_amount ); } ?>
+			</td>
+
+		</tr>
+		<?php } else { ?>
+		<tr>
+			<td width="150">
+				Items being shown:
+			</td>
+			<td width="550">
+				<?php echo str_replace(",",", ", get_option( Tradetracker_productid )); ?>
+			</td>
+
+		</tr>
+		<?php } ?>
+		<tr>
+			<td width="150">
+				Use Lightbox:
+			</td>
+			<td>
+				<?php if (get_option( Tradetracker_lightbox )==0 || get_option( Tradetracker_lightbox )=="") { echo "No"; } else { echo "Yes"; } ?>
+			</td>
+
+		</tr>
+	</table>
+	<hr>
+	If you agree with this you can put <b>[display_store]</b> in any page or post you make. Or use <b>display_store_items()</b> in your theme files.
+	<br>
+	<hr>
+
+			<?php if (get_option( Tradetracker_settings )==1){ ?> 
+				<INPUT type="button" name="Next" value="<?php esc_attr_e('Next') ?>" onclick="location.href='admin.php?page=tradetracker-shop-feedback'"> 
+			<?php } ?>
+		<INPUT type="button" name="Help" value="<?php esc_attr_e('Help') ?>" onclick="location.href='admin.php?page=tradetracker-shop-help#help5'">
+</div>
+	<div id="sideblock" style="float:right;width:200px;margin-left:10px;border:1px;position:relative;border-color:#000000;border-style:solid;"> 
+		<?php news(); ?>
+ 	</div>
+</div>
+<?php
+
+}}
+?>
---
+++ readme.txt
@@ -0,0 +1,67 @@
+=== Plugin Name ===
+Contributors: RPG84
+Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=J3UBRGHKXSAWC
+Tags: tradetracker, store, productfeed, affiliate, generate income, advertisements
+Requires at least: 2.0.1
+Tested up to: 3.2.1
+Stable tag: 2.1.9
+
+A plugin that lets you create a store from the Tradetracker productfeed.
+
+== Description ==
+
+This plugin gives you the abillity to add a store to your WordPress, based on a tradetracker productfeed. Tradetracker is an affiliate system that has
+the abillity to generate a product feed for you. So you can have a store that brings in money without the hassle of owning a complete webstore. All you need to do is choose a store connected on tradetracker and add it. Users of your blog will then see the products on your blog and when interested they will be sent to the store. When they buy an item you will get a percentage of that sale.
+
+Plugin also supports Lightbox. So if you don't have it yet i would advise to install http://wordpress.org/extend/plugins/wp-jquery-lightbox/
+
+== Installation ==
+
+1. Upload `plugin-name.php` to the `/wp-content/plugins/` directory.
+1. Activate the plugin through the 'Plugins' menu in WordPress.
+1. Adjust all settings in the TT Store on the left side of the admin menu
+
+== Frequently Asked Questions ==
+
+= Have you gotten any questions yet? =
+
+No, not yet.
+
+= Why can't I do certain things with the plugin? =
+
+I've created functions for this plugin of which I thought they'd be useful, but ofcourse I may have left things out which other people would like to see. If you have any ideas, do share them with me, as I can always try to implement it.
+
+== Screenshots ==
+
+1. This is how the items will be shown on your site
+2. This is the settings menu in the admin area
+3. Here you select which products you want to show on your site
+
+== Changelog ==
+= 2.1.9 =
+Added the ability to adjust the text on the Buy Now button. The option can be found on the tab settings (basic) or the tab store (advanced) called Buy button text
+
+= 2.1.8 =
+More debugging added. Also improved the importing of an XML a bit so that should go faster.
+
+= 2.1.7 =
+1. Made some changes to the way the layout was shown in the site. It should give less problems now
+1. Added some more error notifications to tell you when something is going wrong.
+
+= 2.1.6 =
+Adjusted some of the CSS for the store. Now your theme should not interfere with the plugins css.
+
+= 2.1.5 =
+Fixed an error for when the XML feed did not use numeric values for there productid
+
+= 2.1.4 =
+Adjusted the price tag. Hopefully higher prices will fit on it now.
+
+= 2.1.3 = 
+Small error fixed that no options where shown when starting the plugin for the first time.
+
+= 2.1.2 = 
+Mostly spelling misstakes and a problem in the Stats on the dashboard.
+
+= 2.1.1 =
+Mostly graphical updates for the admin menu. Also improved the help pages.
