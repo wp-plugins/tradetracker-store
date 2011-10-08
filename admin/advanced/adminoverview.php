7b44135eaf9b19e358ef87cb8cbc540f25e5dc54
RPG84 <RPG84@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-10-08 03:11:43 +0000

    git-svn-id: https://plugins.svn.wordpress.org/tradetracker-store/tags/3.0.5@448665 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ admin/advanced/adminoverview.php
@@ -0,0 +1,223 @@
+<?php
+function format_bytes($size) {
+    $units = array(' B', ' KB', ' MB', ' GB', ' TB');
+    for ($i = 0; $size >= 1024 && $i < 4; $i++) $size /= 1024;
+    return round($size, 2).$units[$i];
+}
+
+function tradetracker_store_overview() {
+global $wpdb;
+$pro_table_prefix=$wpdb->prefix.'tradetracker_';
+$tablemulti = PRO_TABLE_PREFIX."multi";
+$tablelayout = PRO_TABLE_PREFIX."layout";
+define('PRO_TABLE_PREFIX', $pro_table_prefix);
+ttstoreheader();
+	echo '<div class="wrap">';
+	if (get_option("Tradetracker_settings")==2){
+	
+?>
+	<h2>Overview of selected options:</h2>
+<div class="plugindiv">
+<ul class="tabset_tabs">
+   <li><a href="admin.php?page=tradetracker-shop#tab1">Setup</a></li>
+   <li><a href="admin.php?page=tradetracker-shop-settings#tab2">Settings</a></li>
+		<?php if ( get_option("Tradetracker_statsdash") == 1 ) { ?>
+   <li><a href="admin.php?page=tradetracker-shop-stats#tab3">Stats</a></li>
+		<?php } ?>
+   <li><a href="admin.php?page=tradetracker-shop-layout#tab4">Layout</a></li>
+   <li><a href="admin.php?page=tradetracker-shop-multi#tab5">Store</a></li>
+   <li><a href="admin.php?page=tradetracker-shop-multiitems#tab6">Items</a></li>
+   <li><a href="admin.php?page=tradetracker-shop-overview#tab7" class="active">Overview</a></li>
+   <li><a href="admin.php?page=tradetracker-shop-feedback#tab8">Feedback</a></li>
+   <li><a href="admin.php?page=tradetracker-shop-premium#tab9" class="greenpremium">Premium</a></li>
+   <li><a href="admin.php?page=tradetracker-shop-help#tab10" class="redhelp">Help</a></li>
+</ul>
+
+<div id="tab7" class="tabset_content">
+   <h2 class="tabset_label">Overview</h2>
+	<table width="700">
+		<tr>
+			<td width="150">
+				Selected XML File(s):
+			</td>
+			<td>
+				<?php
+					$file = get_option("Tradetracker_xml");
+					$i="1";
+					foreach($file as $filename) {
+						echo "<a href=\"".$filename."\">Click here to open XML</a><br>";
+					}
+				?>
+			</td>
+
+		</tr>
+		<tr>
+			<td width="150">
+				XML File size:
+			</td>
+			<td>
+			<?php
+				$folder =  WP_PLUGIN_DIR . "/tradetracker-store/splits";
+				foreach(glob($folder."/*xml") as $filename) {
+					$xmlsize = format_bytes(filesize($filename))+$xmlsize;
+				}
+				if ($xmlsize!="") {
+					echo round($xmlsize/1024, 2)." MB";
+				} else { 
+					echo "Not calculated yet"; 
+				} 
+			?>
+			</td>
+
+		</tr>
+		<tr>
+			<td width="150">
+				Monthly bandwidth:
+			</td>
+			<td>
+				<?php 				
+					if ($xmlsize!="") {
+						echo round(format_bytes((720/24)*$xmlsize), 2)." MB";
+					} else { 
+						echo "Not calculated yet"; 
+					}
+				?>
+			</td>
+
+		</tr>
+	</table>
+		<hr>
+	<table width="700">
+		<tr>
+			<td width="150">
+				Stats in Dashboard:
+			</td>
+			<td>
+				<?php if (get_option("Tradetracker_statsdash")=="1") { echo "Yes"; } if (get_option("Tradetracker_statsdash")=="0" || get_option("Tradetracker_statsdash")=="") { echo "No"; } ?>
+			</td>
+		</tr>
+		<?php if (get_option("Tradetracker_statsdash")=="1") { ?>
+
+		<tr>
+			<td width="150">
+				CustomerID:
+			</td>
+			<td>
+				<?php echo get_option("Tradetracker_customerid"); ?>
+			</td>
+		</tr>
+
+		<tr>
+			<td width="150">
+				Access code:
+			</td>
+			<td>
+				<?php echo get_option("Tradetracker_access_code"); ?>
+			</td>
+		</tr>
+
+		<tr>
+			<td width="150">
+				Site ID:
+			</td>
+			<td>
+				<?php echo get_option("Tradetracker_siteid"); ?>
+			</td>
+		</tr>
+
+		<tr>
+			<td width="150">
+				Timeframe:
+			</td>
+			<td>
+				<?php if (get_option("Tradetracker_statstime")=="1") { echo "Day"; } if (get_option("Tradetracker_statstime")=="2") { echo "Week"; } if (get_option("Tradetracker_statstime")=="3") { echo "Month"; } ?>
+			</td>
+		</tr>
+		<?php } ?>
+	</table>
+	<hr>
+	<h2>Overview of created stores:</h2>
+<?php	
+		
+	$multi=$wpdb->get_results("SELECT ".$tablemulti.".id as multiid, buynow, layname, multiname, multilayout, multiitems, multiamount, multilightbox FROM ".$tablemulti.",".$tablelayout." where ".$tablemulti.".multilayout=".$tablelayout.".id");
+	foreach ($multi as $multi_val){
+?>
+	<table width="700">
+		<tr>
+			<td width="150">
+				Store Name:
+			</td>
+			<td>
+				<?php echo $multi_val->multiname; ?>
+			</td>
+		</tr>
+		<tr>
+			<td width="150">
+				Layout Name:
+			</td>
+			<td>
+				<?php echo $multi_val->layname; ?>
+			</td>
+		</tr>
+		<tr>
+			<td width="150">
+				Buy now text:
+			</td>
+			<td>
+				<?php echo $multi_val->buynow; ?>
+			</td>
+		</tr>
+		<tr>
+			<td width="150" rowspan="2">
+				Amount of items visible at same time:
+			</td>
+			<td>
+				<?php echo $multi_val->multiamount; ?>
+			</td>
+		</tr>
+		<tr>
+			<td>
+				
+			</td>
+		</tr>
+		<tr>
+			<td width="150">
+				Use Lightbox:
+			</td>
+			<td>
+				<?php if ($multi_val->multilightbox==0 || $multi_val->multilightbox == "") { echo "No"; } else { echo "Yes"; } ?>
+			</td>
+		</tr>
+		<tr>
+			<td width="150">
+				Items to show:
+			</td>
+			<td>
+				<?php echo str_replace(",",", ", $multi_val->multiitems); ?>
+			</td>
+		</tr>
+	</table>
+	<hr>
+	If you agree with this you can put <b>[display_multi store="<?php echo $multi_val->multiid; ?>"]</b> in any page or post you make. Or use <b>display_multi_items($store="<?php echo $multi_val->multiid; ?>")</b> in your theme files.
+	<br>
+	<hr>
+<?php
+	}
+?>
+
+			<?php if (get_option("Tradetracker_settings")==2){ ?> 
+				<INPUT type="button" name="Next" value="<?php esc_attr_e('Next') ?>" onclick="location.href='admin.php?page=tradetracker-shop-feedback'"> 
+			<?php } ?>
+		<INPUT type="button" name="Help" value="<?php esc_attr_e('Help') ?>" onclick="location.href='admin.php?page=tradetracker-shop-help#help8'">
+<?php
+}
+?>
+</div>
+	<div id="sideblock" style="float:right;width:200px;margin-left:10px;border:1px;position:relative;border-color:#000000;border-style:solid;"> 
+		<?php news(); ?>
+ 	</div>
+</div></div>
+
+<?php
+}
+?>
---
+++ readme.txt
@@ -0,0 +1,59 @@
+=== Plugin Name ===
+Contributors: RPG84
+Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=J3UBRGHKXSAWC
+Tags: tradetracker, store, productfeed, affiliate, generate income, advertisements, daisycon, zanox, cleafs
+Requires at least: 2.0.1
+Tested up to: 3.2.1
+Stable tag: 3.0.5
+
+A plugin that lets you create a store from the Tradetracker productfeed.
+
+== Description ==
+
+This plugin gives you the abillity to add a store to your WordPress, based on a tradetracker productfeed. Tradetracker is an affiliate system that has
+the abillity to generate a product feed for you. So you can have a store that brings in money without the hassle of owning a complete webstore. All you need to do is choose a store connected on tradetracker and add it. Users of your blog will then see the products on your blog and when interested they will be sent to the store. When they buy an item you will get a percentage of that sale.
+
+You can also add zanox, daisycon and cleafs With the new premium addons.
+ 
+Plugin also supports Lightbox. So if you don't have it yet i would advise to install http://wordpress.org/extend/plugins/wp-jquery-lightbox/
+
+== Installation ==
+
+1. Upload `tradetracker-store` to the `/wp-content/plugins/` directory.
+1. Activate the plugin through the 'Plugins' menu in WordPress.
+1. Adjust all settings in the TT Store on the left side of the admin menu
+
+== Frequently Asked Questions ==
+
+= Have you gotten any questions yet? =
+
+No, not yet.
+
+= Why can't I do certain things with the plugin? =
+
+I've created functions for this plugin of which I thought they'd be useful, but ofcourse I may have left things out which other people would like to see. If you have any ideas, do share them with me, as I can always try to implement it.
+
+= Do you have a site for the plugin? =
+
+Yes you can find it at http://wpaffiliatefeed.com
+
+== Screenshots ==
+
+1. This is how the items will be shown on your site
+2. This is the settings menu in the admin area
+3. Here you select which products you want to show on your site
+
+== Changelog ==
+= 3.0.5 = 
+- Aligned all text input lengths
+- XML Feed selection is saved
+- Small error in Store creation has been solved. This caused the creation of duplicate stores 
+- You can select categories from items now on the Store tab. It will only show the items from that category on your item selection screen. If you don’t select any item on the item selection screen the plugin will choose random items from that category.
+
+
+= previous version =
+Full changelog on http://wpaffiliatefeed.com/category/releaselog/
+
+== Upgrade Notice ==
+= 3.0.5 = 
+You can now choose the category you would like to use.
