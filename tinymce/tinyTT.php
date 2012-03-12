933ac2425c6240fa3e23d1aacd6786c114c65db4
RPG84 <RPG84@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-03-12 23:36:20 +0000

    git-svn-id: https://plugins.svn.wordpress.org/tradetracker-store/trunk@518173 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- tinymce/tinyTT.php
+++ tinymce/tinyTT.php
@@ -16,7 +16,7 @@ else
 	global $ttstoremultitable;
 ?>
 	<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
-	<script type="text/javascript" src="../../../../../../wp-includes/js/tinymce/tiny_mce_popup.js"></script>
+	<script type="text/javascript" src="<?php bloginfo('wpurl'); ?>/wp-includes/js/tinymce/tiny_mce_popup.js"></script>
 	<script type="text/javascript">
 		var TTTiny = {
 	e: '',
