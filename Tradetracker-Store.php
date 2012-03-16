ec877df349570cd19f2a37f05bf2e08680014549
RPG84 <RPG84@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-03-16 00:53:23 +0000

    git-svn-id: https://plugins.svn.wordpress.org/tradetracker-store/tags/4.0.16@519640 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- Tradetracker-Store.php
+++ Tradetracker-Store.php
@@ -213,6 +213,7 @@ function tradetracker_store_uninstall()
 		delete_option("Tradetracker_premiumapi");
 		delete_option("Tradetracker_adminheight");
 		delete_option("Tradetracker_showurl");
+		delete_option("Tradetracker_searchlayout");
 	}
 }
 ?>
